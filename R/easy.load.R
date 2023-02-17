dda9361cfc77c620ff89b26285a24a696fcfd91b
Jonas Engicht <jonas.engicht@uni-jena.de>
2023-02-17 10:11:34 +0100

    Create easy.load.R

---
+++ R/easy.load.R
@@ -0,0 +1,12 @@
+#' Easy Load
+#'
+#' @param survey_path 
+#' @param experiment.data 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+easy.load = function(survey_path = NA, experiment.data = NA){
+  surv
+}
